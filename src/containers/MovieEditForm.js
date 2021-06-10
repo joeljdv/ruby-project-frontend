898f6ba9497881765cdb3a4d2325b36c4d68b091
joeljdv <joeldiazjdv@gmail.com>
2021-06-10 17:53:22 -0400

    create MovieEditForm file

---
+++ src/containers/MovieEditForm.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class MovieEditForm extends Component {
+    render() {
+        return (
+            <div>
+                working
+            </div>
+        )
+    }
+}
