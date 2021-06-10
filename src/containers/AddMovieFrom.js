264bd798375b5cb0bba740ab282cac2d4e1212e5
joeljdv <joeldiazjdv@gmail.com>
2021-06-10 16:31:26 -0400

    create the form and handlesubmit and handleChange

--- src/containers/AddMovieFrom.js
+++ src/containers/AddMovieFrom.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react'
 
 export default class AddMovieFrom extends Component {
+    state = {
+        name: ''
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+        this.props.addMovie(this.state)
+    }
+
     render() {
         return (
             <div>
-                Working
+                <form onSubmit={this.handleSubmit}>
+                    <label>Name: </label>
+                    <input type="text" name="name" value={this.state.name} onChange={this.handleChange}/>
+                    <input type="submit" />
+                </form>
             </div>
         )
     }
