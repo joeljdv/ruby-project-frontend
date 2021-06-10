77af1053a8b3cd472d6b8d84db0f395ae051dc2a
joeljdv <joeldiazjdv@gmail.com>
2021-06-10 00:15:02 -0400

    create AddDirectorForm and AddMovieForm to add new directors and movies

---
+++ src/containers/AddDirectorForm.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class AddDirectorForm extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
---
+++ src/containers/AddMovieFrom.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class AddMovieFrom extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
