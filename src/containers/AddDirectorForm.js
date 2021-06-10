5730b4bb2408cb6b30e680e2a9c6ba01ae3eaef3
joeljdv <joeldiazjdv@gmail.com>
2021-06-10 15:01:41 -0400

    Create the form and functions to submit new director

--- src/containers/AddDirectorForm.js
+++ src/containers/AddDirectorForm.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react'
 
 export default class AddDirectorForm extends Component {
+
+    state = {
+        name: ''
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+        this.props.addDirector(this.state.name)
+    }
+
     render() {
         return (
             <div>
-                working
+                <form onSubmit={this.handleSubmit}>
+                    <label>Name: </label>
+                    <input type="text" name="name" value={this.state.name} onChange={this.handleChange}/>
+                    <input type="submit" />
+                </form>
             </div>
         )
     }
