2d02b245ba5ba83223a1b7903b7015fb3a7e0624
joeljdv <joeldiazjdv@gmail.com>
2021-06-09 23:47:02 -0400

    create app file and add the routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import Home from './components/Home'
+import Directors from './containers/Directors'
+import Navigation from './components/Navigation'
+import Director from './containers/Director'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+      <Router>
+        <Navigation/>
+        <Switch>
+          <Route  exact path='/' component={Home} />
+          <Route  exact path='/directors' component={Directors}/>
+          <Route  exact path='/directors/:id' component={Director}/>
+        </Switch>
+      </Router> 
     </div>
   );
 }
