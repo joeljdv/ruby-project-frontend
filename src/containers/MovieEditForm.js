c99407710f1c2a41ad918034711152d9906c31f4
joeljdv <joeldiazjdv@gmail.com>
2021-06-10 17:58:30 -0400

    create the edit movie form

--- src/containers/MovieEditForm.js
+++ src/containers/MovieEditForm.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react'
 
 export default class MovieEditForm extends Component {
+    state = {
+        id: null,
+        name: ''
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+        this.props.addMovie(this.state)
+    }
+
     render() {
         return (
             <div>
-                working
+                <form onSubmit={this.handleSubmit}>
+                    <label>Name: </label>
+                    <input type="text" name="name" value={this.state.name} onChange={this.handleChange}/>
+                    <input type="submit" />
+                </form>
             </div>
         )
     }
